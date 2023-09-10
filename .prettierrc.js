aef8efec37f86fbec5dff8f55be55a45d5f17623
cham2 <dearcham2@gmail.com>
2023-09-10 15:52:44 +0900

    🔧  Chore : prettier, lint, husky 설정

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100, // 한 줄의 최대 길이 (100자)
+  singleQuote: true, // 문자열을 작은 따옴표(')로 표시
+  trailingComma: 'es5', // 여러 줄의 배열 또는 객체의 마지막 요소 뒤에 쉼표 추가 ('es5' 또는 'all')
+  semi: true, // 세미콜론 사용
+  tabWidth: 2, // 탭의 너비를 2칸으로 설정
+  bracketSpacing: true, // 객체 리터럴의 중괄호 주위에 공백 추가
+  jsxBracketSameLine: false, // JSX 요소의 시작 태그를 같은 줄에 유지하지 않음
+  arrowParens: 'always', // 화살표 함수의 인자가 하나인 경우 괄호 생략 여부('always' 또는 'avoid')
+};
