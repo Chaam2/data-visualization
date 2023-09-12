82b8781ada7917e5352cf6a1adcfdbb7b68cfe94
cham2 <dearcham2@gmail.com>
2023-09-12 23:28:35 +0900

    ✨ Feat #3 : data의 id값으로 필터링 버튼 생성

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -14,43 +14,54 @@ import { ValueType, NameType } from 'recharts/types/component/DefaultTooltipCont
 
 export const Chart = () => {
   const chartData = useChartData();
+  const uniqueIdsSet = new Set(chartData && chartData.map((data) => data.id));
+  const ids = [...uniqueIdsSet];
 
   return (
-    <ComposedChart
-      width={1200}
-      height={500}
-      data={chartData}
-      margin={{
-        top: 20,
-        right: 20,
-        bottom: 20,
-        left: 20,
-      }}
-    >
-      <CartesianGrid stroke="#f5f5f5" />
-      <XAxis dataKey="time" scale="band" />
-      <YAxis
-        dataKey="value_area"
-        orientation="right"
-        yAxisId="value_area"
-        label={{ value: 'value_area', angle: -90, position: 'insideLeft' }}
-      />
-      <YAxis
-        dataKey="value_bar"
-        yAxisId="value_bar"
-        label={{ value: 'value_bar', angle: -90, position: 'insideLeft' }}
-      />
-      <Tooltip content={<CustomTooltip />} />
-      <Legend />
-      <Bar dataKey="value_bar" barSize={20} fill="#bde24f" yAxisId="value_bar" />
-      <Area
-        type="monotone"
-        dataKey="value_area"
-        fill="#e166a3"
-        stroke="#f06aca"
-        yAxisId="value_area"
-      />
-    </ComposedChart>
+    <>
+      <div>
+        <button>초기화</button>
+        {ids.map((id) => (
+          <button key={id}>{id}</button>
+        ))}
+      </div>
+
+      <ComposedChart
+        width={1200}
+        height={500}
+        data={chartData}
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
+        }}
+      >
+        <CartesianGrid stroke="#f5f5f5" />
+        <XAxis dataKey="time" scale="band" />
+        <YAxis
+          dataKey="value_area"
+          orientation="right"
+          yAxisId="value_area"
+          label={{ value: 'value_area', angle: -90, position: 'insideLeft' }}
+        />
+        <YAxis
+          dataKey="value_bar"
+          yAxisId="value_bar"
+          label={{ value: 'value_bar', angle: -90, position: 'insideLeft' }}
+        />
+        <Tooltip content={<CustomTooltip />} />
+        <Legend />
+        <Bar dataKey="value_bar" barSize={20} fill="#bde24f" yAxisId="value_bar" />
+        <Area
+          type="monotone"
+          dataKey="value_area"
+          fill="#e166a3"
+          stroke="#f06aca"
+          yAxisId="value_area"
+        />
+      </ComposedChart>
+    </>
   );
 };
 
