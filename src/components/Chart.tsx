d22e0eea9110130409e55eca5516e2f14219f988
cham2 <dearcham2@gmail.com>
2023-09-13 16:52:03 +0900

    💄 Design #6 : 차트 색상 설정

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -28,9 +28,15 @@ export const Chart = ({ chartData, selectedId, setSearchParams }: IChartProp) =>
 
   return (
     <div className="flex flex-col gap-4">
-      <div className="self-end">
-        <label htmlFor="date">조회 일자</label>
-        <select id="date" onChange={(e) => setSelectedDate(e.target.value)}>
+      <div className="self-end text-gray-500	">
+        <label htmlFor="date" className="text-sm font-medium mr-1">
+          조회 일자 :{' '}
+        </label>
+        <select
+          id="date"
+          onChange={(e) => setSelectedDate(e.target.value)}
+          className="p-1 rounded-md border-gray-300 border-2 text-sm outline-none	"
+        >
           {dates.map((date) => (
             <option key={date} value={date}>
               {date}
@@ -38,6 +44,7 @@ export const Chart = ({ chartData, selectedId, setSearchParams }: IChartProp) =>
           ))}
         </select>
       </div>
+
       <ResponsiveContainer width="100%" height={600}>
         <ComposedChart
           data={filteredData}
@@ -48,31 +55,38 @@ export const Chart = ({ chartData, selectedId, setSearchParams }: IChartProp) =>
           onClick={handleChartClick}
         >
           <CartesianGrid stroke="#f5f5f5" />
-          <XAxis dataKey="time" scale="auto" angle={-30} tick={{ dy: 20, dx: -30 }} />
+          <XAxis
+            dataKey="time"
+            scale="auto"
+            angle={-30}
+            tick={{ dy: 20, dx: -30, fill: '#999999' }}
+          />
           <YAxis
             dataKey="value_area"
             orientation="right"
             yAxisId="value_area"
-            label={{ value: 'Area', position: 'top', offset: 20 }}
+            label={{ value: 'Area', position: 'top', offset: 20, fill: '#fb7aa3' }}
             domain={[0, 150]}
+            tick={{ fill: '#fb7aa3' }}
           />
           <YAxis
             dataKey="value_bar"
             yAxisId="value_bar"
-            label={{ value: 'Bar', position: 'top', offset: 20 }}
+            label={{ value: 'Bar', position: 'top', offset: 20, fill: '#5ac5f2' }}
+            tick={{ fill: '#5ac5f2' }}
           />
           <Tooltip content={<CustomTooltip />} />
 
           <Bar dataKey="value_bar" barSize={20} yAxisId="value_bar">
             {chartData?.map((data) => (
-              <Cell key={data.id} fill={data.id === selectedId ? '#5ac5f2' : '#bde24f'} />
+              <Cell key={data.id} fill={data.id === selectedId ? '#5ac5f2' : '#5ac4f262'} />
             ))}
           </Bar>
           <Area
             type="monotone"
             dataKey="value_area"
-            fill="#e166a3"
-            stroke="#f06aca"
+            fill="#ffaed2b9"
+            stroke="#fb7aa3"
             yAxisId="value_area"
           />
         </ComposedChart>
